bf5cf1c54217fac5764db8dbb7da8b8e40aae524
Alejandro Campos Palacios <alcampospalacios@gmail.com>
2020-09-16 20:15:56 -0700

    Modified Arquitecture

---
+++ basics.js
@@ -0,0 +1,11 @@
+module.exports = {
+    URLBase : 'https://www.tuenvio.cu/',
+    URLCARLOS3 : 'carlos3/Products?depPid=46095',
+    URL4CAMINOS : '4caminos/Products?depPid=46095',
+    URLTVPEDREGAL : 'tvpedregal/Products?depPid=55',
+    URLTIPICABOLLEROS : 'tipicaboyeros/Products?depPid=46095',
+
+    time: '*/10 * * * * *'
+
+};
+
--- index.js
+++ index.js
@@ -1,14 +1,8 @@
+var basics = require('./basics');
 var request = require('request');
 var cheerio = require('cheerio');
-const cron = require('node-cron');
+var cron = require('node-cron');
 var Task;
-
-const URLBase = 'https://www.tuenvio.cu/';
-const URLCARLOS3 = 'carlos3/Products?depPid=46095';
-const URL4CAMINOS = '4caminos/Products?depPid=46095';
-const URLCIEGO = 'ciego/Products?depPid=46095';
-const URLTVPEDREGAL = 'tvpedregal/Products?depPid=55';
-const URLTIPICABOLLEROS = 'tipicaboyeros/Products?depPid=46095';
 
 function getAlert(urlbase, urlObjective) {
     request(urlbase + urlObjective, (error,
@@ -16,7 +10,6 @@ function getAlert(urlbase, urlObjective) {
         if (!error && response.statusCode == 200) {
             let $ = cheerio.load(html);
             let mainPanel = $('#mainPanel').text();
-
             console.log(mainPanel);
             let adviser = mainPanel.split(" ");
 
@@ -30,38 +23,11 @@ function getAlert(urlbase, urlObjective) {
 
 function initNotifications() {
     alert("Notificaciones Iniciadas");
-    // Comprobamos si ya nos habían dado permiso    **********Por desarrollar********
-    // if (Notification.permission === "granted") {
-
-    //     // Si esta correcto lanzamos la notificación        
-    //     var notification = new Notification("Tu Envio", {
-    //         body: "Servicio de notificacion iniciado correctamente!!"
-    //     });
-    // }
-
-    // // Si no, tendremos que pedir permiso al usuario
-    // else if (Notification.permission !== 'denied') {
-    //     alert("denied");
-    //     Notification.requestPermission(function (permission) {
-    //         // Si el usuario acepta, lanzamos la notificación
-    //         if (permission === "granted") {
-    //             alert("accepted");
-    //             var notification = new Notification("Gracias majo!");
-    //         }
-    //     });
-    // }
-
-    Task = cron.schedule('*/10 * * * * *', async () => {
-        console.log('running a task every 10 seconds URLCARLOS3');
-        await getAlert(URLBase, URLCARLOS3);
-        console.log('running a task every 10 seconds URL4CAMINOS');
-        await getAlert(URLBase, URL4CAMINOS);
-        console.log('running a task every 10 seconds URLCIEGO');
-        await getAlert(URLBase, URLCIEGO);
-        console.log('running a task every 10 seconds URLTVPEDREGAL');
-        await getAlert(URLBase, URLTVPEDREGAL);
-        console.log('running a task every 10 seconds URLTIPICABOLLEROS');
-        await getAlert(URLBase, URLTIPICABOLLEROS);
+    Task = cron.schedule(basics.time, async () => {        
+        await getAlert(basics.URLBase, basics.URLCARLOS3);     
+        await getAlert(basics.URLBase, basics.URL4CAMINOS);      
+        await getAlert(basics.URLBase, basics.URLTVPEDREGAL);        
+        await getAlert(basics.URLBase, basics.URLTIPICABOLLEROS);
     });
 }
 
@@ -92,10 +58,9 @@ chrome.runtime.onInstalled.addListener(function () {
 });
 
 chrome.runtime.onStartup.addListener(function () {
-    console.log("Starting");
     chrome.storage.local.set({ active: false });
     chrome.browserAction.setIcon({ path: 'icon1' + '.png' });
-})
+});
 
 
 chrome.browserAction.onClicked.addListener(currentStatus);
